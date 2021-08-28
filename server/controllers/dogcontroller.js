df787cbeff0229297db6b8dfe3ddf4800a87a267
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-08-28 17:04:25 -0400

    redo

--- server/controllers/dogcontroller.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// ALL OUR CONTROLLERS FOR DOG GO HERE 
-
-module.exports = router
--- server/controllers/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  usercontroller: require('./usercontroller'),
-  dogcontroller: require('./dogcontroller')
-}
