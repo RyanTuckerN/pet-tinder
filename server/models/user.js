89a8302fca343a45e4bd48f5d551cd12d1aaf04c
Marnel Rahimi <marnelrahimi@gmail.com>
2021-09-02 18:26:58 -0500

    user model added, signup endpoint created

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,34 @@
 // USER TABLE MODEL GOES HERE!
+const { DataTypes} = require('sequelize')//bringing in the DataTypes from the sequelize files so that when we type we can select from a pre-selected list of data types
+const sequelize = require('../db')//this is where we connect to the local database
+
+module.exports = User = sequelize.define( 'user', {
+    profile_name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    passwordhash: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false
+    },
+    location: {
+        type: DataTypes.JSON,
+    }
+
+}  
+);
+
+
+
+
+
