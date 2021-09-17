f99d0926048f39232aec5802df1fdf071c9dfc06
Olivia Walton <oliviawalton2298@gmail.com>
2021-09-16 21:32:15 -0400

    updated css on home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,22 +1,32 @@
-import "./Home.css"
-import PetTinderLogo from '../MainLayoutComponents/assets/PetTinderLogo.png'
-// import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import "./Home.css";
+import PetTinderLogo from "../MainLayoutComponents/assets/PetTinderLogo.png";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
 
 const Home = (props) => {
-  return ( 
-    <div>
-      <h1 id="home1">Welcome To Pet Tinder</h1>
+  return (
+    <div className="background">
+      <div id="spacer" /> 
+      <div className="logo-container">
+        <h1 id="home1">Welcome To Pet Tinder</h1>
+        <div className="logo-merge">
+        <img id="doglogo" src={PetTinderLogo} />
+        <ChatBubbleIcon id="chat" />
+        </div>
+        </div>
+      <div className="homepage">
       <h2 id="home2">Where Puppy Love Begins</h2>
       <h3 id="home3">Matching with breeders made easy!</h3>
       <h4 id="home4">Checkout the sidebar to start matching!</h4>
-      <img id="doglogo" src={PetTinderLogo} />
-      <ChatBubbleIcon id="chat" />
+      </div>
+     
+  
+
       <style>
-        @import url('https://fonts.googleapis.com/css2?family=Rampart+One&display=swap');
+        @import
+        url('https://fonts.googleapis.com/css2?family=Rampart+One&display=swap');
       </style>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Home;
